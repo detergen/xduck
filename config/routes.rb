7e0d2e90e7471dd9207ba219d20c8125e16ffc1c
Denis Kolygin <kolygin@gmail.com>
2014-06-05 15:41:26 +0400

    Fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,13 @@
 Xduck::Application.routes.draw do
-devise_scope :user do
-  get "/users/sign_in" => "devise/sessions#new"
-  delete "users/sign_out" => "devise/sessions#destroy"
-end
+  devise_scope :user do
+    get "/users/sign_in" => "devise/sessions#new"
+    delete "users/sign_out" => "devise/sessions#destroy"
+  end
+
   ActiveAdmin.routes(self)
   #resources :articles
 
   devise_for :users
-  get "home/index"
-
-#  root :to => "admin/dashboard#index"
-  #resources :products
-  #resources :boms
-#  root :to => “articles#index”
   get "home/index"
 
   root :to => "home#index"
