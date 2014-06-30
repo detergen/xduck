a9f913fd5f0c9010870c72eaa7251a5426092c82
bgorbunov <gorbunov@akmosoft.com>
2014-06-30 19:36:25 +0400

    basic activity editing

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -16,7 +16,34 @@ class ActivitiesController < ApplicationController
   end
 
   def edit
-    @activity = Activity.find_by :id => params[:id]
+    unless params[:id].nil?
+      @activity = Activity.find_by :id => params[:id]
+    else
+      unless params[:activity][:id].nil?
+        activity = Activity.find_by :id => params[:activity][:id]
+
+        if activity.update(activity_update_params)
+          redirect_to :action => 'show', :parent_id => activity.parent_id
+          return
+        end
+
+        redirect_to :action => 'edit', :id => activity.id
+      end
+    end
+  end
+
+  private
+  def activity_update_params
+    return params.require(:activity).permit(
+        :id,
+        :number,
+        :activity_type_id,
+        :from_organization_id,
+        :to_organization_id,
+        :date,
+        :owner_user_id,
+        :note,
+        :tag)
   end
   
 end
