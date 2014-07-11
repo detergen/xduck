15594ee99f0be5d22b00d65513310fd6d30399de
bgorbunov <gorbunov@akmosoft.com>
2014-07-11 20:46:36 +0400

    minor changes

--- app/controllers/activity_items_controller.rb
+++ app/controllers/activity_items_controller.rb
@@ -1,6 +1,19 @@
 class ActivityItemsController < ApplicationController
   before_action :authenticate_user!
 
+  def add
+    if request.get?
+      unless params[:product_id].nil? && params[:quantity].nil?
+        activity_item = ActivityItem.create(
+            :product_id => params[:product_id],
+            :quantity => params[:quantity]
+        )
+
+        activity_item.save()
+      end
+    end
+  end
+
   def delete
     if request.get?
       unless params[:id].nil?
