6d603229274db25202dcbc2ceba680e54ed11e45
Denis <den-stepa@yandex.ru>
2015-05-03 21:50:39 +0300

    fixing key attr in permited_params

--- app/admin/addr.rb
+++ app/admin/addr.rb
@@ -1,17 +1,5 @@
 ActiveAdmin.register Addr do
-permit_params     :name, :typeofaddr, :postindex, :string1, :string2, :key, :note, :organization_id, :contact_id
-  
-  # See permitted parameters documentation:
-  # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # permit_params :list, :of, :attributes, :on, :model
-  #
-  # or
-  #
-  # permit_params do
-  #  permitted = [:permitted, :attributes]
-  #  permitted << :other if resource.something?
-  #  permitted
-  # end
+  permit_params :name, :typeofaddr, :postindex, :string1, :string2,
+                :address_key, :note, :organization_id, :contact_id
   
 end
