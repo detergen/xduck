2dcc6689e13c7e1fc5e814faa4ab1b0bf7faa8bc
Denis Stepanov <den-stepa@yandex.ru>
2017-10-11 12:20:15 +0200

    update autoload paths

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,8 @@ module Xduck
       g.fixture_replacement :factory_girl, :dir => "spec/factories"
     end
 
-    config.autoload_paths += Dir["#{config.root}/lib/**/"]
+    config.autoload_paths << "#{config.root}/lib"
+    config.autoload_paths << "#{config.root}/lib/**/"
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
