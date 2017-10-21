5ebb73f4021cba79cce6706bf298480866d9ce0b
Denis Stepanov <den-stepa@yandex.ru>
2017-10-21 21:09:28 +0200

    fix jquery conflicts

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,7 @@
-//= require jquery3
+//= require jquery2
 //= require jquery_ujs
 //= require jquery.validate
 //= require wice_grid
 //= require twitter/bootstrap
 //= require select2-full
-//= require_tree .
+//= require_tree ./application
